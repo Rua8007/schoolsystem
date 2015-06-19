50b54efaa74c76d6c726b030c0c951b06f6e94f1
omer5161 <omerfarooq5161@gmail.com>
2015-06-19 22:54:37 +0500

    theme initial

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,27 @@ module Schoolsystem
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "easypiechart")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "sparklines")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "form-toggle")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "fullcalendar")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "form-daterangepicker")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "charts-flot")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "pulsate")
+    config.assets.paths << Rails.root.join("app", "assets", "demo")
+    config.assets.paths << Rails.root.join("app", "assets", "demo", "variations")
+    config.assets.paths << Rails.root.join("app", "assets", "plugins", "form-markdown", "css")
+
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "codeprettifier")
+    # config.assets.paths << Rails.root.join("app", "assets", "plugins", "bootstrap","js")
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
