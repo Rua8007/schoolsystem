1b8308cca495d0c6d330d5574a6e99eda3201e70
I'm Genius <faisal.softengg@outlook.com>
2015-06-21 17:58:46 +0500

    student form added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,18 @@
 class StudentsController < ApplicationController
+	def index
+		@students = Student.all
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		
+	end
+
+	private
+    def create_params
+      params.require(:student).permit(:first_name, :mobile, :address, :email, :grade_id, :dob,:gender,:middle_name, :last_name, :blood, :birth_place, :nationality, :language, :religion, :city, :state, :country,:phone, :fee, :term, :dueDate)      
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620203411) do
+ActiveRecord::Schema.define(version: 20150621122554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20150620203411) do
     t.string   "phone"
     t.string   "email"
     t.string   "parent_id"
+    t.integer  "fee"
+    t.string   "term"
+    t.string   "dueDate"
   end
 
   create_table "users", force: :cascade do |t|
