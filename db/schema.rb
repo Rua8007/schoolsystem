31799128409cbaaeebab0590ebaf01ff179e575f
Zaeem <zaeem.asif@gmail.com>
2015-07-01 22:44:20 +0500

    first commit after merging conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,41 @@ ActiveRecord::Schema.define(version: 20150630185016) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string   "employee_number"
+    t.date     "date_of_joining"
+    t.string   "full_name"
+    t.string   "gender"
+    t.date     "date_of_birth"
+    t.string   "religion"
+    t.string   "qualification"
+    t.integer  "employee_category_id"
+    t.integer  "employee_department_id"
+    t.string   "marital_status"
+    t.integer  "child_count"
+    t.string   "father_name"
+    t.string   "mother_name"
+    t.string   "spouse_name"
+    t.string   "blood_group"
+    t.string   "nationality"
+    t.string   "id_card_no"
+    t.date     "id_card_expiry"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "city"
+    t.string   "country"
+    t.string   "home_phone"
+    t.string   "mobile_number"
+    t.string   "email"
+    t.float    "salary"
+    t.integer  "employee_position_id"
+    t.string   "status"
+    t.date     "pay_date"
+    t.date     "next_due_date"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "parents", force: :cascade do |t|
     t.string   "name"
     t.string   "relation"
@@ -96,40 +131,5 @@ ActiveRecord::Schema.define(version: 20150630185016) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "employees", force: :cascade do |t|
-    t.string   "employee_number"
-    t.date     "date_of_joining"
-    t.string   "full_name"
-    t.string   "gender"
-    t.date     "date_of_birth"
-    t.string   "religion"
-    t.string   "qualification"
-    t.integer  "employee_category_id"
-    t.integer  "employee_department_id"
-    t.string   "marital_status"
-    t.integer  "child_count"
-    t.string   "father_name"
-    t.string   "mother_name"
-    t.string   "spouse_name"
-    t.string   "blood_group"
-    t.string   "nationality"
-    t.string   "id_card_no"
-    t.date     "id_card_expiry"
-    t.string   "address1"
-    t.string   "address2"
-    t.string   "city"
-    t.string   "country"
-    t.string   "home_phone"
-    t.string   "mobile_number"
-    t.string   "email"
-    t.float    "salary"
-    t.integer  "employee_position_id"
-    t.string   "status"
-    t.date     "pay_date"
-    t.date     "next_due_date"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
 
 end
