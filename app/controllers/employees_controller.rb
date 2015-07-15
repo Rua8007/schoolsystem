9bc9926d90ae6d7974c223ac405ea264b9ed9fa1
omer5161 <omerfarooq5161@gmail.com>
2015-07-16 00:02:07 +0500

    employee design

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -25,6 +25,7 @@ class EmployeesController < ApplicationController
     @categories = Category.all
     @departments = Department.all
     @positions = Position.all
+    @employee = Employee.find(params[:id])
   end
 
   # POST /employees
@@ -34,7 +35,7 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully created.' }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new }
@@ -48,7 +49,7 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully updated.' }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit }
@@ -62,7 +63,7 @@ class EmployeesController < ApplicationController
   def destroy
     @employee.destroy
     respond_to do |format|
-      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
+      format.html { redirect_to employees_path, notice: 'Employee was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714161245) do
+ActiveRecord::Schema.define(version: 20150715184402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,8 +88,10 @@ ActiveRecord::Schema.define(version: 20150714161245) do
     t.string   "status"
     t.date     "pay_date"
     t.date     "next_due_date"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "total_experience_years"
+    t.integer  "total_experience_months"
   end
 
   create_table "leaves", force: :cascade do |t|
