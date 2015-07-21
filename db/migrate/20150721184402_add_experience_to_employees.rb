37630a3fae54f58bc2a0507ecddaf7e9b1f17293
omer5161 <omerfarooq5161@gmail.com>
2015-07-21 16:58:08 +0500

    deleted additional migration

--- db/migrate/20150721184402_add_experience_to_employees.rb
+++
@@ -1,6 +0,0 @@
-class AddExperienceToEmployees < ActiveRecord::Migration
-  def change
-  	add_column :employees, :total_experience_years, :integer
-  	add_column :employees, :total_experience_months, :integer
-  end
-end
