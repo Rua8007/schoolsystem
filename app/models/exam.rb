e824335d225f07c1b6b20e42f337d67f8ba061c7
nauman <nauman.goldsmith@gmail.com>
2015-12-24 17:44:23 +0500

    Added Validation on Quarter For a Single grade.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,4 +2,16 @@ class Exam < ActiveRecord::Base
 	belongs_to :batch
   has_many :marksheets
 	has_many :sessionals
+
+	before_create :unique_exam
+
+	def unique_exam
+		if Exam.where(grade_id: self.grade_id, name: self.name, batch_id: self.batch_id).present?
+      errors.add(:name, "#{self.name} is already present for this grade.")
+      false
+    else
+      true
+    end
+  end
+
 end
