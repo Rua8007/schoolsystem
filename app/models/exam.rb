ff36a8fec18ae7b44ab5812a41eb5c9c63efdb25
nauman <nauman.goldsmith@gmail.com>
2015-12-24 17:57:38 +0500

    Fixed Issue In Validation.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,10 +3,11 @@ class Exam < ActiveRecord::Base
   has_many :marksheets
 	has_many :sessionals
 
-	before_create :unique_exam
+	before_save :unique_exam
 
 	def unique_exam
-		if Exam.where(grade_id: self.grade_id, name: self.name, batch_id: self.batch_id).present?
+    exam = Exam.find_by(grade_id: self.grade_id, name: self.name, batch_id: self.batch_id)
+		if exam.present? and exam.id != self.id
       errors.add(:name, "#{self.name} is already present for this grade.")
       false
     else
