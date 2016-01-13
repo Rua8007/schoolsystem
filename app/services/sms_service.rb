28c67a279bb11fd1114d3eb48b7ced2f5e04edf3
nauman <nauman.goldsmith@gmail.com>
2016-01-13 23:24:14 +0500

    Fixed Issue In Sms Sending.

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -3,10 +3,13 @@ class SmsService
     # response = HTTParty.post('http://dreamsms.net/sendHexEncoded.HTML?UserName=test9&Password=123&senderName=Al-omam&message=testing&MobileNo=+923134145612&txtlang=1')
     response = HTTParty.post("http://rest.dreamsms.net/sms",
               {
-                :body => {:UserName => 'nation', :Password => '123', :SenderName => 'Al Omam International', :Message => body, :Mobiles => send_to}.to_json,
+                :body => {:UserName => 'nations', :Password => '123', :SenderName => 'Al Omam International', :Message => body, :Mobiles => send_to}.to_json,
                 :headers => { 'Content-Type' => " application/json", 'accept' => 'application/json charset=utf-8'}
               })
     # return render json: response
     # http://dreamsms.net/sendHexEncoded.HTML?UserName=test9&Password=123&senderName=Drea mSMS&message=test&MobileNo=541847555&txtlang=1
+    puts '============================================'
+    puts response
+    puts '============================================'
   end
 end
