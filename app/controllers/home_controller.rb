ee40e52af89a13102ea8bfaa5870f0934165ab4b
nauman <nauman.goldsmith@gmail.com>
2016-01-16 17:16:55 +0500

    Completed Teacher's Dashboard.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,9 @@ class HomeController < ApplicationController
       @student_no = @student.rollnumber
 
       @bridges = Bridge.where(grade_id: @student.grade_id) if @student.present?
+    elsif current_user.role.name == 'Teacher'
+      @teacher = Employee.find_by_email(current_user.email)
+      @bridges = Bridge.where(employee_id: @teacher.id) if @teacher.present?
     end
   end
 
