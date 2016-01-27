a43699ab46fc52ee7918ddd647439862d1f51de6
nauman <nauman.goldsmith@gmail.com>
2016-01-28 00:17:25 +0500

    Fixed Email Format.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '52.26.245.82' }
 end
