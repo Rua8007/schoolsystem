87bf47de2161ffe9618ae2caefe7c23c1464e36c
nauman <nauman.goldsmith@gmail.com>
2016-02-03 23:48:35 +0500

    Added Message Templates To Sms.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,45 +29,7 @@ class HomeController < ApplicationController
     end
 
     if params[:sms]
-      send_to = ''
-      if params[:all]
-        number = Employee.all.pluck(:mobile_number) + Parent.all.pluck(:mobile)
-        send_to = number.join(',')
-        result = SmsService.new.delay.sendsms(params[:msgbdy], send_to)
-      else
-        if params[:parent].present?
-          parents_numbers = Parent.all.pluck(:mobile).join(',')
-          result = SmsService.new.delay.sendsms(params[:msgbdy], parents_numbers)
-        end
-
-        if params[:staff].present?
-          employee_numbers = Employee.all.pluck(:mobile_number).join(',')
-          result = SmsService.new.delay.sendsms(params[:msgbdy], employee_numbers)
-        end
-
-        student_numbers = []
-        if params[:student].present?
-          student_numbers = []
-          student_ids = params[:students]
-          student_ids.try(:each) do |std_id|
-            std = Student.find(std_id)
-            student_numbers << std.mobile if std.present?
-          end
-          student_numbers = student_numbers.compact.join(',')
-          result = SmsService.new.delay.sendsms(params[:msgbdy], student_numbers) if student_numbers.present?
-        end
-
-        if params[:grade].present?
-          student_numbers = []
-          grade_ids = params[:grades]
-          grade_ids.try(:each) do |grade_id|
-            students = Student.where(grade_id: grade_id)
-            student_numbers = student_numbers + students.pluck(:mobile) if students.present?
-          end
-          student_numbers = student_numbers.compact.join(',')
-          result = SmsService.new.delay.sendsms(params[:msgbdy], student_numbers) if student_numbers.present?
-        end
-      end
+      SmsService.new(params).delay.send_sms
     end
     flash[:notice] = 'Notifications sent.'
     redirect_to home_sms_path()
