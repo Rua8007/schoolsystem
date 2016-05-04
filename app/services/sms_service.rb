52f670b28b2708e6df43d7a880f94c5d6bd60ad6
Im Genius <im.genius.man@gmail.com>
2016-05-04 13:17:35 +0500

    change sender name

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -16,7 +16,7 @@ class SmsService
     # response = HTTParty.post('http://dreamsms.net/sendHexEncoded.HTML?UserName=test9&Password=123&senderName=Al-omam&message=testing&MobileNo=+923134145612&txtlang=1')
     response = HTTParty.post("http://rest.dreamsms.net/sms",
               {
-                :body => {:UserName => 'nations', :Password => '123', :SenderName => 'Al Omam International', :Message => body, :Mobiles => send_to}.to_json,
+                :body => {:UserName => 'nations', :Password => '123', :SenderName => 'AlOmamSch', :Message => body, :Mobiles => send_to}.to_json,
                 :headers => { 'Content-Type' => " application/json", 'accept' => 'application/json charset=utf-8'}
               })
     # return render json: response
