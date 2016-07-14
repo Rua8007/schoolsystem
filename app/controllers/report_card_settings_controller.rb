acf385ddf38de49db3b068f5aabbd451ff63367e
Usman <mail.usmanasif@gmail.com>
2016-07-14 17:56:32 +0500

    error in take exam if

--- app/controllers/report_card_settings_controller.rb
+++ app/controllers/report_card_settings_controller.rb
@@ -111,10 +111,12 @@ class ReportCardSettingsController < ApplicationController
             subject = ReportCardSubject.find(id)
             subject.update(parent_id: @subject.id)
 
-            if params[:take_exam].present? && params[:take_exam].include? subject.id
-              subject.update(parent_id: @subject.id, take_exam: true)
-            else
-              subject.update(parent_id: @subject.id, take_exam: false)
+            if params[:take_exam].present?
+              if params[:take_exam].include? subject.id
+                subject.update(parent_id: @subject.id, take_exam: true)
+              else
+                subject.update(parent_id: @subject.id, take_exam: false)
+              end
             end
           end
        end
