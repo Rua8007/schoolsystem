fb6d4d842835f9713cc7bd108e4a95cfca441e1f
Faisal Waleed <faisal.softengg@outlook.com>
2016-10-16 17:24:19 +0500

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -49,11 +49,11 @@ class HomeController < ApplicationController
   end
 
   def backups
-    if !current_user.role.rights.where(value: "access_backups").any? || !session[:confirm_password] == true
+    if current_user.role.rights.find_by_value(value: "access_backups").nil? || session[:confirm_password] == false
       
       puts "========backups nh dkhany aya========== "
-      puts 
-      puts
+      puts current_user.role.rights.find_by_value(value: "access_backups").nil?
+      puts session[:confirm_password]
       puts
       redirect_to root_path, alert: "Sorry! You are not authorized"
     else
